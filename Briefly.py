435e03cb84278c696df7275d8b20c2321e8623ee
Mhijazi16 <gultimate0@gmail.com>
2024-06-18 13:51:50 +0300

    adding header and chat_input

--- Briefly.py
+++ Briefly.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 def main(): 
     st.set_page_config(page_icon=":scroll:", page_title="Briefly")
+    st.header("Chat With Your Documents :scroll:")
+    st.chat_input("Ask Question !")    
 
 if __name__ == '__main__': 
     main()
