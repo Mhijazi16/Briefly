147b33c0fbe04092c729b499894fe8dc349c5ed5
Mhijazi16 <gultimate0@gmail.com>
2024-06-20 14:08:56 +0300

    adding header and text input

--- Briefly.py
+++ Briefly.py
@@ -1,7 +1,9 @@
 import streamlit as st 
 
 def main(): 
-    pass
+    st.set_page_config(page_icon=":scroll:",page_title="Briefly")
+    st.header("Chat With Your Documents :scroll:")
+    input = st.text_input("Enter Question !")
 
 if __name__ == "__main__": 
     main()
