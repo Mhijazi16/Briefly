af2cb5a27c3c5ce961a57155835eb9b05e7ccfbb
Mhijazi16 <gultimate0@gmail.com>
2024-06-20 14:10:05 +0300

    adding button, file_uploader and subheader

--- Briefly.py
+++ Briefly.py
@@ -5,5 +5,10 @@ def main():
     st.header("Chat With Your Documents :scroll:")
     input = st.text_input("Enter Question !")
 
+    with st.sidebar: 
+        st.subheader("Upload Your Documents")
+        st.file_uploader("upload your files here", accept_multiple_files=True)
+        st.button(":package: Process")
+
 if __name__ == "__main__": 
     main()
